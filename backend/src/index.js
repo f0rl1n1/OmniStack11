e0134caea18f78978ed19f64e26bfd0c95b910ed
f0rl1n1 <fabio.forlini@gmail.com>
2020-03-26 17:30:32 -0300

    Back-End da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no banck-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Excluir uma informação no back-end
+ */
+
+ /**
+  * Tipo de parâmetros:
+  * 
+  * Query Params: Parâmetros nomeados após a rota iniciando com "?"(/Users?name=Forlini)
+  * Route Params: Parâmetros utilizados para identificar um único recurso (/Users/:id)
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar uma informação
+  */
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
